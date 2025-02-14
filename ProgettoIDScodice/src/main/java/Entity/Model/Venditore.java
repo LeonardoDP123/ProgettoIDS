e24fd357bca087dd3454cba6259fe8ed3c22e1d9
AlessioBoninti <alessio.boninti@studenti.unicam.it>
2025-02-14 18:37:49 +0100

    Modificata classe VENDITORE

--- ProgettoIDScodice/src/main/java/Entity/Model/Venditore.java
+++ ProgettoIDScodice/src/main/java/Entity/Model/Venditore.java
@@ -4,15 +4,33 @@ import Entity.Controller.Ruolo;
 
 import java.time.LocalDate;
 
+
+import java.time.LocalDate;
+
 /**
- * Classe astratta per un venditore (Produttore, Trasformatore, DistributoreTipicità).
+ * Classe astratta per i venditori (Produttore, Trasformatore, DistributoreTipicità).
  */
 public abstract class Venditore extends User {
-	public Venditore(int id, String username, String nome, String cognome, LocalDate dataDiNascita,
+	public Venditore(int ID, String username, String nome, String cognome, LocalDate dataDiNascita,
 					 String numeroDiTelefono, String indirizzo) {
-		super(id, username, nome, cognome, dataDiNascita, numeroDiTelefono, indirizzo, Ruolo.VENDITORE);
+		super(ID, username, nome, cognome, dataDiNascita, numeroDiTelefono, indirizzo, Ruolo.VENDITORE);
 	}
 
-	public abstract void aggiungiProdotto();
-	public abstract void rimuoviProdotto();
+
+	public abstract void aggiungiProdotto(Prodotto prodotto);
+	public abstract void rimuoviProdotto(Prodotto prodotto);
+
+
+	public void modificaProdotto(Prodotto prodotto, String nuovoNome, String nuovaDescrizione, int nuovaQuantita) {
+		prodotto.setNome(nuovoNome);
+		prodotto.setDescrizione(nuovaDescrizione);
+		prodotto.setQuantitaDisponibile(nuovaQuantita);
+		System.out.println("Prodotto modificato: " + prodotto.getNome());
+	}
+
+	@Override
+	public void mostraDettagli() {
+		System.out.println("Venditore: " + getNome() + " " + getCognome() +
+				" | Username: " + getUsername() + " | Telefono: " + getNumeroDiTelefono());
+	}
 }
